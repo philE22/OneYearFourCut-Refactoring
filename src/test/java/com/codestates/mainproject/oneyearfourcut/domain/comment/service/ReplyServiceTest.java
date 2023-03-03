e2cc6a9a977e98b424a7a550bdc4d10513e35fe8
philE22 <moonwhlking@gmail.com>
2023-03-03 15:40:16 +0900

    Delete : 유닛 테스트 삭제

--- src/test/java/com/codestates/mainproject/oneyearfourcut/domain/comment/service/ReplyServiceTest.java
+++
@@ -1,8 +0,0 @@
-package com.codestates.mainproject.oneyearfourcut.domain.comment.service;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-@ExtendWith(MockitoExtension.class)
-class ReplyServiceTest {
-}
